5b22e6c7e6dd121e2987307f66327f2fdb5a71da
lauramerinoortiz <lauramerinoortiz.guadalupe@alumnado.fundacionloyola.net>
2023-03-10 10:06:58 +0100

    Niveles actualizados

--- src/modelos/nivel.js
+++ src/modelos/nivel.js
@@ -2,7 +2,7 @@ const nivel =[
     {
         nivel:1,
         texto:"Soy un texto ignaciano1",
-        imagen:"../assets/img/ignacio1.png",
+        imagen:"../src/assets/img/ignacio1.png",
         numPiezas:"4",
         x:437,
         y:310
@@ -10,7 +10,7 @@ const nivel =[
     {
         nivel:2,
         texto:"Soy un texto ignaciano2",
-        imagen:"../assets/img/ignacio2.png",
+        imagen:"../src/assets/img/ignacio2.png",
         numPiezas:"4",
         x:435,
         y:306
@@ -18,7 +18,7 @@ const nivel =[
     {
         nivel:3,
         texto:"En el tiempo de convalecencia, leyó los libros La vida de Cristo, del cartujo Ludolfo de Sajonia, y el Flos Sanctorum, que hicieron mella en él. Bajo la influencia de esos libros, se replanteó toda la vida e hizo autocrítica de su vida como soldado. Este deseo se vio acrecentado por una visión de la Virgen con el Niño Jesús, que provocó la definitiva conversión del soldado en religioso. De allí salió con la convicción de viajar a Jerusalén con la tarea de la conversión de los no cristianos en Tierra Santa.",
-        imagen:"../assets/img/ignacio3.png",
+        imagen:"../src/assets/img/ignacio3.png",
         numPiezas:"4",
         x:444,
         y:308
@@ -26,7 +26,7 @@ const nivel =[
     {
         nivel:4,
         texto:"Soy un texto ignaciano4",
-        imagen:"../assets/img/ignacio4.png",
+        imagen:"../src/assets/img/ignacio4.png",
         numPiezas:"4",
         x:434,
         y:311
@@ -34,7 +34,7 @@ const nivel =[
     {
         nivel:5,
         texto:"Soy un texto ignaciano5",
-        imagen:"../assets/img/ignacio5.png",
+        imagen:"../src/assets/img/ignacio5.png",
         numPiezas:"9",
         x:293,
         y:206
@@ -42,7 +42,7 @@ const nivel =[
     {
         nivel:6,
         texto:"Soy un texto ignaciano6",
-        imagen:"../assets/img/ignacio6.png",
+        imagen:"../src/assets/img/ignacio6.png",
         numPiezas:"9",
         x:300,
         y:203
@@ -50,15 +50,15 @@ const nivel =[
     {
         nivel:7,
         texto:"Su amiga Isabel Roser le dio el consejo de que empezase a estudiar. San Ignacio aprendío latín y se apuntó a la universidad. Estudió en Alcalá de Henares desde el año 1526 hasta 1527; estuvo viviendo y trabajando en el Hospital de Antezana como enfermero y como cocinero para los enfermos.",
-        imagen:"../assets/img/ignacio7.png",
+        imagen:"../src/assets/img/ignacio7.png",
         numPiezas:"9",
         x:296,
         y:206
     },
     {
         nivel:8,
-        texto:"Soy un texto ignaciano8",
-        imagen:"../assets/img/ignacio8.png",
+        texto:"Posteriormente, en julio de 1527, Ignacio fue a Salamanca, hablando a todos sobre sus ejercicios espirituales, cosa que no fue bien vista por las autoridades y le acarreó algunos problemas, y lo llegaron a encarcelar, el santo estuvo en la cárcel salmantina 22 días. En vista de la falta de libertad para su plática en España, decidió estudiar en París. Hacia mediados de septiembre de 1527, 15 o 20 días después de salir de la cárcel, Ignacio partió hacía París llevando unos libros en un asnillo.",
+        imagen:"../src/assets/img/ignacio8.png",
         numPiezas:"9",
         x:294,
         y:206
@@ -66,7 +66,7 @@ const nivel =[
     {
         nivel:9,
         texto:"En 1528 San Ignacio entró en la Universidad de París, donde estuvo más de 7 años, para aumentar su educación teológica y literaria, tratando de enseñar a los estudiantes sus Ejercicios Espirituales. Los Ejercicios Espirituales es un libro con oraciones y meditaciones para hacer durante 30 días y así acercar a la persona hacia Dios.",
-        imagen:"../assets/img/ignacio9.png",
+        imagen:"../src/assets/img/ignacio9.png",
         numPiezas:"16",
         x:219,
         y:152
@@ -74,7 +74,7 @@ const nivel =[
     {
         nivel:10,
         texto:"Soy un texto ignaciano10",
-        imagen:"../assets/img/ignacio10.png",
+        imagen:"../src/assets/img/ignacio10.png",
         numPiezas:"16",
         x:227,
         y:154
@@ -82,7 +82,7 @@ const nivel =[
     {
         nivel:11,
         texto:"Soy un texto ignaciano11",
-        imagen:"../assets/img/ignacio11.png",
+        imagen:"../src/assets/img/ignacio11.png",
         numPiezas:"16",
         x:220,
         y:155
@@ -90,7 +90,7 @@ const nivel =[
     {
         nivel:12,
         texto:"Soy un texto ignaciano12",
-        imagen:"../assets/img/ignacio12.png",
+        imagen:"../src/assets/img/ignacio12.png",
         numPiezas:"16",
         x:227,
         y:153
@@ -98,7 +98,7 @@ const nivel =[
     {
         nivel:13,
         texto:"Soy un texto ignaciano13",
-        imagen:"../assets/img/ignacio13.png",
+        imagen:"../src/assets/img/ignacio13.png",
         numPiezas:"25",
         x:182,
         y:133
@@ -106,7 +106,7 @@ const nivel =[
     {
         nivel:14,
         texto:"Soy un texto ignaciano14",
-        imagen:"../assets/img/ignacio14.png",
+        imagen:"../src/assets/img/ignacio14.png",
         numPiezas:"25",
         x:177,
         y:120
@@ -114,15 +114,15 @@ const nivel =[
     {
         nivel:15,
         texto:"En octubre de 1538, Ignacio se encaminó hacia Roma, junto con Fabre y Laínez, para la aprobación de la constitución de la nueva orden. Un grupo de cardenales se mostró a favor de la constitución y Paulo III confirmó la orden mediante la bula Regimini militantis (27 de septiembre de 1540), pero limitaba el número de sus miembros a sesenta. Esta limitación fue revocada a través de la bula Injunctum nobis (14 de marzo de 1543). Así nacía la Societas Iesu, la Compañía de Jesús o, como se le conoce comúnmente, «los Jesuitas».",
-        imagen:"../assets/img/ignacio15.png",
+        imagen:"../src/assets/img/ignacio15.png",
         numPiezas:"25",
-        x:111,
-        y:111
+        x:175,
+        y:131
     },
     {
         nivel:16,
         texto:"Soy un texto ignaciano16",
-        imagen:"../assets/img/ignacio16.png",
+        imagen:"../src/assets/img/ignacio16.png",
         numPiezas:"25",
         x:111,
         y:111
